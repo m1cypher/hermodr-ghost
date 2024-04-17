ae007ff3f435e4c1ba868feb01bae34a225320ac
Odin <odin@boydsbar.xyz>
2024-04-17 16:58:17 -0500

    Working on content collection.

--- repository-scanning.py
+++ repository-scanning.py
@@ -1,10 +1,13 @@
+# https://docs.github.com/en/rest?apiVersion=2022-11-28
+
 import requests
-from io import StringIO
 import os
 from dotenv import load_dotenv
 from pprint import pprint
 from datetime import datetime
-
+from github import Github, Auth
+import base64
+from bs4 import BeautifulSoup
 
 
 
@@ -25,6 +28,7 @@ content_response = requests.get(url=GITHUB_REPO_PATH_CONTENTS, headers=HEADERS)
 content_response.raise_for_status()
 
 content_data = content_response.json()
+# print(content_data)
 
 review_dictionary = {}
 
@@ -33,14 +37,30 @@ for review in content_data:
     review_path = review['path']
     review_dictionary[review_name.strip('.md')] = {'name': review_name, 'path': review_path}
 
-commit_response = requests.get(url=GITHUB_REPO_PATH_COMMITS, headers=HEADERS)
-commit_response.raise_for_status()
 
-commit_data = commit_response.json()
+# Working to pull commit time, but only shows entire repo. Wanted specific file. See #Content-frontmatter for attempted new direction.
+# commit_response = requests.get(url=GITHUB_REPO_PATH_COMMITS, headers=HEADERS)
+# commit_response.raise_for_status()
 
-for commit in commit_data:
-    print(commit['commit']['committer']['date'])
+# commit_data = commit_response.json()
+# # review_dictionary[review_name.strip('.md')]
+# # print(commit_data[0]['commit']['committer']['date'])
+# pprint(commit_data)
 
+auth = Auth.Token(GITHUB_API_KEY)
+github = Github(auth=auth)
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+
+content_repo = github.get_user().get_repo("obsidian-notes")
+file_content = content_repo.get_contents("03 - Projects/Media Reviews/TV Shows/Brooklyn Nine-Nine.md")
+content = file_content.content
+cleantext = BeautifulSoup(base64.b64decode(content), "lxml").text
+
+if "status: complete" in cleantext:
+    print("Completed review")
+else:
+    print("This is not ready")
 
 # pprint(review_dictionary)
 
