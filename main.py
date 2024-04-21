359570b04e9cc1223d54ae8d77b232128d5fb33f
Odin <odin@boydsbar.xyz>
2024-04-20 20:20:33 -0500

    started main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+
+logging=basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='hermodr.log'
+)
+
