bc7623ca1fce8efa7d42cf7af569970a3c33039d
Odin <odin@boydsbar.xyz>
2024-04-24 18:02:49 -0500

    Initial creation.

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 import logging
 from dotenv import load_dotenv
 
+from ghostAdmin import GhostAdmin  
 
-logging=basicConfig(
+
+logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     filename='hermodr.log'
 )
 
+
+GhostAdmin.getMembers()
