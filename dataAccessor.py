abda873a813caac287d61bd18e2aa78275c605ee
Odin <odin@boydsbar.xyz>
2024-04-28 18:43:42 -0500

    Started process to create SQL like json DB.

---
+++ dataAccessor.py
@@ -0,0 +1,27 @@
+# https://github.com/pysonDB/pysonDB?tab=readme-ov-file
+
+
+class dataAccessor():
+
+    def __init__(self) -> None:
+        self.db = db
+
+        db = {}
+
+    def get():
+        db = db.getDb(data.json)
+        return db
+
+    def delete():
+        pass
+
+    
+    def create():
+        pass
+
+    
+    def update():
+        pass
+
+    def commit():
+        pass
