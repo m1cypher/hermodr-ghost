9eef6e072b038865128ca83144ce89814eae71f9
Odin <odin@boydsbar.xyz>
2024-05-03 16:07:45 -0500

    Added baseline, but it will change.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import logging
 from dotenv import load_dotenv
+from pprint import pprint
 
-from ghostAdmin import GhostAdmin  
+from ghostAdmin import GhostAdmin
+from repositoryScanning import GithubReviewAdmin
 
 
 logging.basicConfig(
@@ -12,5 +14,18 @@ logging.basicConfig(
     filename='hermodr.log'
 )
 
+def main():
+    load_dotenv()
+    GITHUB_API_KEY = os.getenv("GITHUB_API_KEY")
 
-GhostAdmin.getMembers()
+    updater = GithubReviewAdmin(GITHUB_API_KEY)
+
+    tv_accessor = updater.process_reviews("TV Shows", "TV")
+    movie_accessor = updater.process_reviews("Movies", "Movie")
+    book_accessor = updater.process_reviews("Books", "Book")
+
+    review_dc = {"TV": tv_accessor, "Movies": movie_accessor, "Books": book_accessor}
+    pprint(review_dc)
+
+if __name__ == "__main__":
+    main()
