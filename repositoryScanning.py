350b24d9b7f4854112dcf513e07f10048bf8fc16
Odin <odin@boydsbar.xyz>
2024-05-07 16:47:26 -0500

    working to correct the new information pull and the dataAccessor tinyDB inputs.

--- repositoryScanning.py
+++ repositoryScanning.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from dataAccessor import dataAccessor
 
 
+
 class GithubReviewAdmin:
     
     def __init__(self, github_api_key):
@@ -30,42 +31,55 @@ class GithubReviewAdmin:
 
     def update_review_status(self, accessor, repo_name, file_path, key, value):
         content_repo = self.github.get_user().get_repo(repo_name)
-        for review_file in accessor.search("type", value):
-            file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
-            content = file_content.content
-            cleantext = base64.b64decode(content).decode("utf-8")
-            cleantext = BeautifulSoup(cleantext, "lxml").text
-            for line in cleantext.split('\n'):
-                status_index = line.find(f"{key}: ")
-                if status_index != -1:
-                    status = line[status_index + len(f"{key}: "):].strip()
-                    accessor.update_item_by_id(review_file['id'], {value: status})
+        print(content_repo)
+        review_files = accessor.search_item("name", value)
+        if review_files:
+            for review_file in review_files:
+                file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
+                content = file_content.content
+                cleantext = base64.b64decode(content).decode("utf-8")
+                cleantext = BeautifulSoup(cleantext, "lxml").text
+                for line in cleantext.split('\n'):
+                    status_index = line.find(f"{key}: ")
+                    if status_index != -1:
+                        status = line[status_index + len(f"{key}: "):].strip()
+                        accessor.update_item_by_id(review_file['type'], review_file.doc_id, {"status": status})
+        else:
+            print(f"No {value} found.")
 
     def update_imdb_info(self, accessor, repo_name, file_path, key, value):
         content_repo = self.github.get_user().get_repo(repo_name)
-        for review_file in accessor.search("type", value):
-            file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
-            content = file_content.content
-            cleantext = base64.b64decode(content).decode("utf-8")
-            cleantext = BeautifulSoup(cleantext, "lxml").text
-            for line in cleantext.split('\n'):
-                status_index = line.find(f"{key}: ")
-                if status_index != -1:
-                    imdbId = line[status_index + len(f"{key}: "):].strip()
-                    accessor.update_item_by_id(review_file['id'], {value: imdbId})
+        review_files = accessor.search_item("name", value)
+        if review_files:
+            for review_file in review_files:
+                file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
+                content = file_content.content
+                cleantext = base64.b64decode(content).decode("utf-8")
+                cleantext = BeautifulSoup(cleantext, "lxml").text
+                for line in cleantext.split('\n'):
+                    status_index = line.find(f"{key}: ")
+                    if status_index != -1:
+                        imdbId = line[status_index + len(f"{key}: "):].strip()
+                        accessor.update_item_by_id(review_file['type'], review_file.doc_id, {"imdbId": imdbId})
+        else:
+            print(f"No {value} found.")
 
     def update_isbn_info(self, accessor, repo_name, file_path, key, value):
         content_repo = self.github.get_user().get_repo(repo_name)
-        for review_file in accessor.search("type", value):
-            file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
-            content = file_content.content
-            cleantext = base64.b64decode(content).decode("utf-8")
-            cleantext = BeautifulSoup(cleantext, "lxml").text
-            for line in cleantext.split('\n'):
-                isbn_index = line.find(f"{key}: ")
-                if isbn_index != -1:
-                    isbn = line[isbn_index + len(f"{key}: "):].strip()
-                    accessor.update_item_by_id(review_file['id'], {value: isbn})
+        review_files = accessor.search_item("name", value)
+        if review_files:
+            for review_file in review_files:
+                file_content = content_repo.get_contents(f"{file_path}/{review_file['path']}.md")
+                content = file_content.content
+                cleantext = base64.b64decode(content).decode("utf-8")
+                cleantext = BeautifulSoup(cleantext, "lxml").text
+                for line in cleantext.split('\n'):
+                    isbn_index = line.find(f"{key}: ")
+                    if isbn_index != -1:
+                        isbn = line[isbn_index + len(f"{key}: "):].strip()
+                        accessor.update_item_by_id(review_file['type'], review_file.doc_id, {"isbn": isbn})
+        else:
+            print(f"No {value} found.")
 
     def process_reviews(self, path, review_type):
         review_path = f"03 - Projects/Media Reviews/{path}"
@@ -73,13 +87,14 @@ class GithubReviewAdmin:
         review_old = self.fetch_content('obsidian-notes', review_path)
         for review in review_old:
             name, path = self.extract_review_details(review, review_path)
-            if review_type == "TV":
+            if review_type == "tv":
                 accessor.add_tv_show(name, path, "", "", "")
-            elif review_type == "Movie":
+            elif review_type == "movie":
                 accessor.add_movie(name, path, "", "", "")
-            elif review_type == "Book":
+            elif review_type == "book":
                 accessor.add_book(name, path, "", "", "")
         self.update_review_status(accessor, "obsidian-notes", review_path, "status", "status")
         self.update_imdb_info(accessor, "obsidian-notes", review_path, "imbdId", "imdbId")
         self.update_isbn_info(accessor, "obsidian-notes", review_path, "isbn", "isbnId")
         return accessor
+
