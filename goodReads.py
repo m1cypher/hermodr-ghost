ba9c4feb2cf0f8fb32208b4567d7ac1862fb6cd1
Odin <odin@boydsbar.xyz>
2024-05-09 17:19:34 -0500

    Staged files that will need to be made in the future.

---
+++ goodReads.py
@@ -0,0 +1,5 @@
+# Planning for future automation. Saving for notes
+# https://github.com/maria-antoniak/goodreads-scraper
+# https://abdelmalekmerouan.medium.com/how-i-scraped-goodreads-51e045355a6f
+# https://stackoverflow.com/questions/68697683/struggling-to-log-in-to-goodreads-using-requests-session-what-is-missing-in-my
+
---
+++ imageSearch.py
@@ -0,0 +1,4 @@
+# https://github.com/wagnerrp/pytmdb3/ for TV and Movies
+# Scrape goodreads for <img class"ResponseiveImage"...> for the link. 
+
+
